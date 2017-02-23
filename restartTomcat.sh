ba075ce7d08d9e6b1ee4132fe8eb0b0104219486
Tejas Parikh <tejas@tejasparikh.com>
2017-02-23 17:38:11 -0500

    Script updates

--- restartTomcat.sh
+++ restartTomcat.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-sudo service tomcat7 restart
+sudo service tomcat7 stop
+cd /var/lib/tomcat7/webapps
+sudo rm -rf test-1.0.0
+sudo service tomcat7 start
--- src/main/java/com/csye6225/HelloWorldServlet.java
+++ src/main/java/com/csye6225/HelloWorldServlet.java
@@ -12,6 +12,6 @@ public class HelloWorldServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        resp.getOutputStream().write("Hello World 123.".getBytes());
+        resp.getOutputStream().write("Hello World 456.".getBytes());
     }
 }
