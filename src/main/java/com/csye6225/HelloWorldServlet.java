7ba6248f920a1067f19bfcc7cb5c5ca40852b98d
Tejas Parikh <tejas@tejasparikh.com>
2017-03-30 13:31:18 -0400

    Added logging

--- src/main/java/com/csye6225/HelloWorldServlet.java
+++ src/main/java/com/csye6225/HelloWorldServlet.java
@@ -6,12 +6,33 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.joda.time.DateTime;
 
 public class HelloWorldServlet extends HttpServlet {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloWorldServlet.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        resp.getOutputStream().write("Hello World 890.".getBytes());
+        DateTime dateTime = new DateTime();
+
+        String message = "Current date time is " + dateTime;
+
+        LOGGER.info("This is INFO log message");
+        LOGGER.warn("This is WARN log message");
+        LOGGER.error("This is ERROR log message");
+        LOGGER.debug("This is DEBUG log message");
+
+//        try{
+//            throw new Exception("This is dummy exception.");
+//        } catch (Exception e) {
+//            LOGGER.error(e.getMessage(),e);
+//        }
+
+        resp.getOutputStream().write(message.getBytes());
     }
 }
